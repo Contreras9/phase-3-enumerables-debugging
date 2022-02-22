216c67983db267f770cbdfb43a35a7eb78042dd2
Hamzah Contreras <Hamzah_36@outlook.com>
2022-02-22 11:09:55 -0600

    commit tree e135623334dadeab2d94307ebfed54660a7562a5

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,25 @@
+require 'pry'
+
 # Write your code here
+
+# def reverse_each_word(sentence)
+#     words = sentence.split
+#     reversed_words = []
+  
+#     words.each do |word|
+#       reversed_words << word.reverse
+#     end
+  
+#     reversed_words.join(" ")
+#   end
+  
+#   binding.pry
+#   0
+
+def reverse_each_word(sentence)
+    words = sentence.split
+  
+    reversed_words = words.map(&:reverse).join(" ")
+  end
+  
+  puts reverse_each_word("Hello there, and how are you?")
